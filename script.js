7c81254eb91931f14bd9d9ce6879147ffb2207da
gorcsvra <66862598+gorcsvra@users.noreply.github.com>
2021-05-15 23:21:42 +0200

    a html és a css-bár nem jó kész, a js nem működik még

---
+++ script.js
@@ -0,0 +1,13 @@
+setInterval(setClock, 1000)
+queryS
+const hourHand =document.querySelector('[data<div 
+    <strong>Warning!</strong> <a href="#" class="alert-link"></a>
+</div>')
+
+function setClock() {
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 
+    60
+    const hoursRatio = (minutesRatio + currentDate.getHours() / 12
+}
