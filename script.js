1e5b9fac2dd754e50a5e81e87c882a091912164f
gorcsvra <66862598+gorcsvra@users.noreply.github.com>
2021-05-16 22:52:25 +0200

    kész, de nem mőködik)

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 setInterval(setClock, 1000)
-queryS
-const hourHand =document.querySelector('[data<div 
-    <strong>Warning!</strong> <a href="#" class="alert-link"></a>
-</div>')
 
+const hourHand =document.querySelector('[data-hour-hand}')
+const minuteHand =document.querySelector('[data-minute-hand]')
+const secondHand =document.querySelector('[data-second-hand]')
+   
 function setClock() {
     const currentDate = new Date()
     const secondsRatio = currentDate.getSeconds() / 60
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 
     60
-    const hoursRatio = (minutesRatio + currentDate.getHours() / 12
+    const hoursRatio = (minutesRatio + currentDate.getHours() / 12)
+
+setRotation(secondHand, secondsRatio)
+setRotation(minutesHand, minutesRatio)
+setRotation(hoursHand, hoursRatio)
+
+
 }
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('rotation', rotationRatio*360)
+
+}
+
+setClock()
